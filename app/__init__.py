db0c0dfbd70612764e677483604ca3289ceca83e
seon <vosej7930@gmail.com>
2024-08-10 20:49:34 +0900

    __init__.py로 파일명 변경

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,11 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
     
-    #SQLAlchemy 초기화
+    # 블루프린트 등록
+    from .routes.face_recognition_routes import face_recognition_bp
+    app.register_blueprint(face_recognition_bp)
+
+    # SQLAlchemy 초기화
     with app.app_context():
         from .models import User, AcssLog, ErrorLog, Interview, Question, Response, Report, ImageFile, VideoFile
         db.create_all()
