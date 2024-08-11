3a7199a0abc34e87859c76a8a9bda526ac3bc08f
seon <vosej7930@gmail.com>
2024-08-11 21:07:21 +0900

    녹음

--- app/routes/face_recognition_routes.py
+++ app/routes/face_recognition_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template, request, Response
+import os
+from flask import Blueprint, render_template, request, Response, send_file, abort
 import cv2
 import pyaudio
 import wave
@@ -6,8 +7,10 @@ import threading
 
 face_recognition_bp = Blueprint('face_recognition', __name__)
 
-# 카메라 프레임 생성기
-def gen_frames(): 
+is_recording = False
+output_filename = os.path.join(os.path.dirname(__file__), 'output.wav')  # 절대 경로로 설정
+
+def gen_frames():  # 카메라 프레임 생성기
     cap = cv2.VideoCapture(0)
     while True:
         success, frame = cap.read()
@@ -23,7 +26,9 @@ def gen_frames():
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-def record_audio(output_filename="output.wav", record_seconds=5):
+def record_audio(output_filename="output.wav"):
+    global is_recording
+
     chunk = 1024
     format = pyaudio.paInt16
     channels = 1
@@ -39,9 +44,11 @@ def record_audio(output_filename="output.wav", record_seconds=5):
 
     frames = []
 
-    for i in range(0, int(rate / chunk * record_seconds)):
+    print("Recording started...")
+    while is_recording:
         data = stream.read(chunk)
         frames.append(data)
+    print("Recording stopped.")
 
     stream.stop_stream()
     stream.close()
@@ -56,10 +63,25 @@ def record_audio(output_filename="output.wav", record_seconds=5):
 
 @face_recognition_bp.route('/start_audio_recording', methods=['POST'])
 def start_audio_recording():
-    t = threading.Thread(target=record_audio)
+    global is_recording
+    is_recording = True
+    t = threading.Thread(target=record_audio, args=(output_filename,))
     t.start()
     return "Recording started"
 
+@face_recognition_bp.route('/stop_audio_recording', methods=['POST'])
+def stop_audio_recording():
+    global is_recording
+    is_recording = False
+    return "Recording stopped"
+
+@face_recognition_bp.route('/play_audio')
+def play_audio():
+    if os.path.exists(output_filename):
+        return send_file(output_filename, as_attachment=False)
+    else:
+        return abort(404, description="File not found")
+    
 @face_recognition_bp.route('/')
 def index():
     return render_template('face_recognition.html')
