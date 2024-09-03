3da606e490a1e3ad915ec3a2a8a38f888d49dc10
chaewon <snnigiyong@gmail.com>
2024-09-03 20:17:33 +0900

    조채원_메인페이지,css,js,img추가

---
+++ app/static/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+
+  //slider
+    $('.owl-carousel').owlCarousel({
+    center: false,
+    loop:false,
+    margin:24,
+    nav:false,
+    responsive:{
+      0:{
+          items:4
+      }}
+    });
+
+
+})
+
