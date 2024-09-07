212b7de46ca5b37dd93614b79f9ebc25fa123630
seon <vosej7930@gmail.com>
2024-09-07 11:20:23 +0900

    김경선_공통된 script,css분리 / 모달 css 수정

---
+++ app/static/js/common.js
@@ -0,0 +1,34 @@
+$(function() {
+  // 모달 열기 버튼을 클릭하면 해당 모달을 보여줌
+  $(".open_modal").click(function() {
+    console.log("???");
+    const modalId = $(this).data("modal");  
+    $("#" + modalId).show();
+    console.log(modalId);
+    openModal();
+  });
+
+  // 모달 닫기 버튼을 클릭하면 해당 모달을 숨김
+  $(".modal .close").click(function() {
+    $(this).closest(".modal").hide();
+    closeModal();
+  });
+
+  // 모달 외부 클릭 시 모달 닫기
+  $(".modal").click(function(event) {
+    if ($(event.target).is(".modal")) {  
+      $(this).hide();
+      closeModal();
+    }
+  });
+
+  // 모달을 열 때
+  function openModal() {
+    $("body").css("overflow", "hidden");  
+  }
+
+  // 모달을 닫을 때
+  function closeModal() {
+    $("body").css("overflow", "");  
+  }
+});
