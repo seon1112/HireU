580fa40221c49b72f1397f312ba05b3b9de497c8
seon <vosej7930@gmail.com>
2024-09-07 10:02:50 +0900

    김경선_header,sidebar template 분리

--- app/routes/main_routes.py
+++ app/routes/main_routes.py
@@ -3,5 +3,6 @@ from flask import Blueprint, render_template
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
+@main_bp.route('/main')
 def main():
     return render_template('view_main.html')
