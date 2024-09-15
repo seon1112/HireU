0c0f9f5f86bb2f34e3d27feee4c8e193f0000527
seon <vosej7930@gmail.com>
2024-09-15 11:06:28 +0900

    김경선_모달 닫을 때 선택 정보 초기화

--- app/static/js/make_question.js
+++ app/static/js/make_question.js
@@ -22,4 +22,17 @@ $(function() {
     closeModal();
   });
 
+  $("#historyModal").click(function(event) {
+    if ($(event.target).is(".modal")) {  
+      $(this).hide();
+      closeHistoryModal();
+      closeModal();
+    }
+  });
 });
+
+/*history모달 닫을 시 기존 선택 정보 모두 삭제 */
+function closeHistoryModal(){
+  $(".question_list").removeClass('show');
+  $(".question_list > .question_item > input[type='checkbox']").prop('checked',false);
+}
